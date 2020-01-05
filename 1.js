94d94cef55eaa2cb5f6bf853a7a53cc94694b69e
Your Name <you@example.com>
2020-01-05 17:38:27 +0900

    add js

--- 1.js
+++ 1.js
@@ -1,10 +1,15 @@
 $(function(){
   var vitrikhoiedu = $('#khoiedu').offset().top;
-  vitrikhoiedu = vitrikhoiedu -40;
+   vitrikhoiedu = vitrikhoiedu -40;
+  var vitricommit = $('#commit').offset().top;
+
   //console.log(vitrikhoiedu);
 
   $('.xuong, .kn').click(function(event) {
-
     $('html,body').animate({scrollTop:vitrikhoiedu}, 700);
+  });
+  $('.cm').click(function(event) {
+  /* Act on the event */
+    $('html,body').animate({scrollTop:vitricommit}, 700);
   });
 })
